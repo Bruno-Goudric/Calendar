f0964f9bfef4295e5da0ee71a2e82797bc2999fc
Bruno Ferreira <brunolfsports@hotmail.com>
2022-08-26 18:06:25 -0300

    feat: Create Card Days

--- src/components/Organisms/Calendar/index.tsx
+++ src/components/Organisms/Calendar/index.tsx
@@ -1,9 +1,14 @@
 import React from 'react';
 
+import * as Molecules from 'components/Molecules';
 import * as S from './styles';
 
 function Calendar() {
-  return <div>index</div>;
+  return (
+    <S.Container>
+      <Molecules.Card />
+    </S.Container>
+  );
 }
 
 export { Calendar };
--- src/pages/Home/index.tsx
+++ src/pages/Home/index.tsx
@@ -1,12 +1,14 @@
 import React from 'react';
 
 import * as Molecules from 'components/Molecules';
+import * as Organisms from 'components/Organisms';
 import * as S from './styles';
 
 function Home() {
   return (
     <S.Container>
       <Molecules.Header />
+      <Organisms.Calendar />
     </S.Container>
   );
 }
